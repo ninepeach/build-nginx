b18ec68325fa75393ac0e74a8af44a55badde37d
Stone Magic <stone@Stones-MBP14.local>
2024-11-03 00:21:22 +0800

    update

--- build-nginx.sh
+++ build-nginx.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 # Run as root or with sudo
-if [[ $EUID -ne 0 ]]; then
+if [ $EUID -ne 0 ]; then
   echo "This script must be run as root or with sudo."
   exit 1
 fi
 
-# Ensure curl is installed
-#apt-get update && apt-get install curl -y
+# Ensure curl and required packages are installed
+apt-get update && apt-get install curl -y
+mkdir -p build
 
 # Set URLs to the source directories
 source_pcre=https://onboardcloud.dl.sourceforge.net/project/pcre/pcre/8.45/
@@ -23,88 +24,59 @@ version_nginx=nginx-1.26.2
 # Set OpenPGP keys used to sign downloads
 opgp_pcre=45F68D54BBE23FB3039B46E59766E084FB0F43D8
 opgp_zlib=5ED46A6721D365587791E2AA783FCD8E58BCAFBA
-opgp_openssl_1=8657ABB260F056B1E5190839D9C4D26D0E604491 #Matt Caswell
-opgp_openssl_2=B7C1C14360F353A36862E4D5231C84CDDCC69C45 #Paul Dale
-opgp_openssl_3=7953AC1FBC3DC8B3B292393ED5E9E43F7DF9EE8C #Richaard Levitte
-opgp_openssl_4=A21FAB74B0088AA361152586B8EF1A6BA9DA2D5C #Tomas Mrax
-opgp_openssl_5=EFC0A467D613CB83C7ED6D30D894E2CE8B3D79F5 #OpenSSL OMC
+opgp_openssl_1=8657ABB260F056B1E5190839D9C4D26D0E604491
+opgp_openssl_2=B7C1C14360F353A36862E4D5231C84CDDCC69C45
+opgp_openssl_3=7953AC1FBC3DC8B3B292393ED5E9E43F7DF9EE8C
+opgp_openssl_4=A21FAB74B0088AA361152586B8EF1A6BA9DA2D5C
+opgp_openssl_5=EFC0A467D613CB83C7ED6D30D894E2CE8B3D79F5
 opgp_nginx=13C82A63B603576156E30A4EA0EA981B66B0D967
 
 # Set where OpenSSL and NGINX will be built
 bpath=$(pwd)/build
+srcPath=$(pwd)/src
 install_path=/usr/local/nginx
 
 # Make a "today" variable for use in back-up filenames later
 today=$(date +"%Y-%m-%d")
 
-# Ensure the required software to compile NGINX is installed
+# Ensure the required software to compile NGINX and PHP are installed
 apt-get -y install \
   binutils \
   build-essential \
   curl \
   dirmngr \
-  libssl-dev
+  libssl-dev \
+  php-fpm \
+  php-cli \
+  php-mysql
 
 # Download the source files
-if [ -f "${bpath}/${version_pcre}.tar.gz" ]; then
-    echo "${version_pcre}.tar.gz"  already downloaded
+if [ -f "${srcPath}/${version_pcre}.tar.gz" ]; then
+    echo "${version_pcre}.tar.gz" already downloaded
 else
-    echo curl -L "${source_pcre}${version_pcre}.tar.gz" -o "${bpath}/${version_pcre}.tar.gz"
-    curl -L "${source_pcre}${version_pcre}.tar.gz" -o "${bpath}/${version_pcre}.tar.gz"
+    curl -L "${source_pcre}${version_pcre}.tar.gz" -o "${srcPath}/${version_pcre}.tar.gz"
 fi
 
-if [ -f "${bpath}/${version_zlib}.tar.gz" ]; then
-    echo "${version_zlib}.tar.gz"  already downloaded
+if [ -f "${srcPath}/${version_zlib}.tar.gz" ]; then
+    echo "${version_zlib}.tar.gz" already downloaded
 else
-    echo curl -L "${source_zlib}${version_zlib}.tar.gz" -o "${bpath}/${version_zlib}.tar.gz"
-    curl -L "${source_zlib}${version_zlib}.tar.gz" -o "${bpath}/${version_zlib}.tar.gz"
+    curl -L "${source_zlib}${version_zlib}.tar.gz" -o "${srcPath}/${version_zlib}.tar.gz"
 fi
 
-if [ -f "${bpath}/${version_openssl}.tar.gz" ]; then
-    echo "${version_openssl}.tar.gz"  already downloaded
+if [ -f "${srcPath}/${version_openssl}.tar.gz" ]; then
+    echo "${version_openssl}.tar.gz" already downloaded
 else
-    echo curl -L "${source_openssl}${version_openssl}.tar.gz" -o "${bpath}/${version_openssl}.tar.gz"
-    curl -L "${source_openssl}${version_openssl}.tar.gz" -o "${bpath}/${version_openssl}.tar.gz"
+    curl -L "${source_openssl}${version_openssl}.tar.gz" -o "${srcPath}/${version_openssl}.tar.gz"
 fi
 
-if [ -f "${bpath}/${version_nginx}.tar.gz" ]; then
-    echo "${version_nginx}.tar.gz"  already downloaded
+if [ -f "${srcPath}/${version_nginx}.tar.gz" ]; then
+    echo "${version_nginx}.tar.gz" already downloaded
 else
-    echo curl -L "${source_nginx}${version_nginx}.tar.gz" -o "${bpath}/${version_nginx}.tar.gz"
-    curl -L "${source_nginx}${version_nginx}.tar.gz" -o "${bpath}/${version_nginx}.tar.gz"
+    curl -L "${source_nginx}${version_nginx}.tar.gz" -o "${srcPath}/${version_nginx}.tar.gz"
 fi
 
-# Download the signature files
-if [ -f "${bpath}/${version_pcre}.tar.gz.sig" ]; then
-    echo "${version_pcre}.tar.gz.sig"  already downloaded
-else
-    echo curl -L "${source_pcre}${version_pcre}.tar.gz.sig" -o "${bpath}/${version_pcre}.tar.gz.sig"
-    curl -L "${source_pcre}${version_pcre}.tar.gz.sig" -o "${bpath}/${version_pcre}.tar.gz.sig"
-fi
-
-if [ -f "${bpath}/${version_zlib}.tar.gz.asc" ]; then
-    echo "${version_zlib}.tar.gz.asc"  already downloaded
-else
-    echo curl -L "${source_zlib}${version_zlib}.tar.gz.asc" -o "${bpath}/${version_zlib}.tar.gz.asc"
-    curl -L "${source_zlib}${version_zlib}.tar.gz.asc" -o "${bpath}/${version_zlib}.tar.gz.asc"
-fi
-
-if [ -f "${bpath}/${version_openssl}.tar.gz.asc" ]; then
-    echo "${version_openssl}.tar.gz.asc"  already downloaded
-else
-    echo curl -L "${source_openssl}${version_openssl}.tar.gz.asc" -o "${bpath}/${version_openssl}.tar.gz.asc"
-    curl -L "${source_openssl}${version_openssl}.tar.gz.asc" -o "${bpath}/${version_openssl}.tar.gz.asc"
-fi
-
-if [ -f "${bpath}/${version_nginx}.tar.gz.asc" ]; then
-    echo "${version_nginx}.tar.gz.asc"  already downloaded
-else
-    echo curl -L "${source_nginx}${version_nginx}.tar.gz.asc" -o "${bpath}/${version_nginx}.tar.gz.asc"
-    curl -L "${source_nginx}${version_nginx}.tar.gz.asc" -o "${bpath}/${version_nginx}.tar.gz.asc"
-fi
-
-# Verify the integrity and authenticity of the source files through their OpenPGP signature
-cd "$bpath"
+# Verify the integrity and authenticity of the source files
+cd "$srcPath"
 GNUPGHOME="$(mktemp -d)"
 export GNUPGHOME
 gpg --keyserver keyserver.ubuntu.com --recv-keys "$opgp_pcre" "$opgp_zlib" "$opgp_openssl_1" "$opgp_openssl_2" "$opgp_openssl_3" "$opgp_openssl_4" "$opgp_openssl_5" "$opgp_nginx"
@@ -113,7 +85,7 @@ gpg --batch --verify ${version_zlib}.tar.gz.asc ${version_zlib}.tar.gz
 gpg --batch --verify ${version_openssl}.tar.gz.asc ${version_openssl}.tar.gz
 gpg --batch --verify ${version_nginx}.tar.gz.asc ${version_nginx}.tar.gz
 
-
+cd "$bpath"
 # Clean up source files
 rm -rf \
   "$bpath"/${version_zlib} \
@@ -122,44 +94,18 @@ rm -rf \
   "$bpath"/${version_nginx}
 
 # Expand the source files
-cd "$bpath"
-for archive in ./*.tar.gz; do
+for archive in ../src/*.tar.gz; do
   tar xzf "$archive"
-  echo tar xzf "$archive"
 done
 
-
-# Clean up source files
+# Clean up GNUPGHOME
 rm -rf \
   "$GNUPGHOME"
 
-# Rename the existing /etc/nginx directory so it's saved as a back-up
-if [ -d "${install_path}/etc/nginx" ]; then
-  mv ${install_path}/etc/nginx "${install_path}/etc/nginx-${today}"
-fi
-
-# Create NGINX cache directories if they do not already exist
-if [ ! -d "${install_path}/var/cache/nginx/" ]; then
-  mkdir -p \
-    ${install_path}/var/cache/nginx/client_temp \
-    ${install_path}/var/cache/nginx/proxy_temp \
-    ${install_path}/var/cache/nginx/fastcgi_temp \
-    ${install_path}/var/cache/nginx/uwsgi_temp \
-    ${install_path}/var/cache/nginx/scgi_temp
-fi
-
-# Test to see if our version of gcc supports __SIZEOF_INT128__
-if gcc -dM -E - </dev/null | grep -q __SIZEOF_INT128__
-then
-  ecflag="enable-ec_nistp_64_gcc_128"
-else
-  ecflag=""
-fi
-
-# Build NGINX, with various modules included/excluded
+# Build NGINX with PHP-FPM support
 cd "$bpath/$version_nginx"
 ./configure \
-  --prefix=${install_path}/etc/nginx \
+  --prefix=${install_path} \
   --with-cc-opt="-O3 -fPIE -fstack-protector-strong -Wformat -Werror=format-security" \
   --with-ld-opt="-Wl,-Bsymbolic-functions -Wl,-z,relro" \
   --with-pcre="$bpath/$version_pcre" \
@@ -169,15 +115,15 @@ cd "$bpath/$version_nginx"
   --sbin-path=${install_path}/sbin/nginx \
   --modules-path=${install_path}/lib/nginx/modules \
   --conf-path=${install_path}/etc/nginx/nginx.conf \
-  --error-log-path=${install_path}/var/log/nginx/error.log \
-  --http-log-path=${install_path}/var/log/nginx/access.log \
-  --pid-path=${install_path}/var/run/nginx.pid \
-  --lock-path=${install_path}/var/run/nginx.lock \
-  --http-client-body-temp-path=${install_path}/var/cache/nginx/client_temp \
-  --http-proxy-temp-path=${install_path}/var/cache/nginx/proxy_temp \
-  --http-fastcgi-temp-path=${install_path}/var/cache/nginx/fastcgi_temp \
-  --http-uwsgi-temp-path=${install_path}/var/cache/nginx/uwsgi_temp \
-  --http-scgi-temp-path=${install_path}/var/cache/nginx/scgi_temp \
+  --error-log-path=${install_path}/logs/error.log \
+  --http-log-path=${install_path}/logs/access.log \
+  --pid-path=${install_path}/run/nginx.pid \
+  --lock-path=${install_path}/run/nginx.lock \
+  --http-client-body-temp-path=${install_path}/cache/client_temp \
+  --http-proxy-temp-path=${install_path}/cache/proxy_temp \
+  --http-fastcgi-temp-path=${install_path}/cache/fastcgi_temp \
+  --http-uwsgi-temp-path=${install_path}/cache/uwsgi_temp \
+  --http-scgi-temp-path=${install_path}/cache/scgi_temp \
   --user=nginx \
   --group=nginx \
   --with-file-aio \
@@ -208,30 +154,125 @@ make install
 make clean
 strip -s ${install_path}/sbin/nginx*
 
-if [ -d "${install_path}/etc/nginx-${today}" ]; then
-  # Rename the default /etc/nginx settings directory so it's accessible as a reference to the new NGINX defaults
-  mv ${install_path}/etc/nginx ${install_path}/etc/nginx-default
+mkdir -p ${install_path}/etc/nginx/conf.d
+mkdir -p ${install_path}/logs
+mkdir -p ${install_path}/cache
+mkdir -p ${install_path}/service
 
-  # Restore the previous version of /etc/nginx to /etc/nginx so the old settings are kept
-  mv "${install_path}/etc/nginx-${today}" ${install_path}/etc/nginx
+# PHP-FPM Configuration
+echo "Configuring PHP-FPM..."
+
+cat <<EOF > ${install_path}/etc/nginx/nginx.conf
+user  nginx;
+worker_processes auto;                   # Automatically select worker processes based on CPU
+worker_rlimit_nofile 100000;              # Max file descriptors per worker
+error_log logs/error.log crit;
+
+events {
+    worker_connections 8192;              # Max number of simultaneous connections per worker
+    multi_accept on;                      # Accept multiple connections at once
+    use epoll;                            # Use epoll for Linux
+}
+
+http {
+
+    include       mime.types;
+    default_type  application/octet-stream;
+
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
+                      '"\$http_user_agent" "\$http_x_forwarded_for"';
+
+    #access_log  logs/access.log  main;
+    access_log /dev/null;
+
+    # General Optimizations
+    sendfile on;                          # Enables efficient file transfer
+    tcp_nopush on;                        # Send headers in one packet
+    tcp_nodelay on;                       # Minimize latency for keep-alive connections
+    keepalive_timeout 65;                 # Timeout for keep-alive connections
+    types_hash_max_size 2048;             # Increase MIME type hash table size
+
+    # Buffers and Timeouts
+    client_body_buffer_size 10K;
+    client_header_buffer_size 1k;
+    large_client_header_buffers 4 16k;    # For handling large headers
+    output_buffers 1 512k;
+    postpone_output 1460;
+    reset_timedout_connection on;
+
+    # Gzip Compression
+    gzip on;
+    gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
+    gzip_min_length 256;
+    gzip_comp_level 5;
+    gzip_vary on;
+
+    ##
+    # Virtual Host Configs
+    ##
+
+    include conf.d/*.conf;
+
+}
+EOF
+
+cat <<EOF > ${install_path}/etc/nginx/conf.d/default.conf
+server {
+    listen       80;
+    server_name  localhost;
+
+    access_log  logs/access.log  main;
+
+    location / {
+        root   html;
+        index  index.html index.htm index.php;
+    }
+
+    error_page  404              /404.html;
+    error_page  500 502 503 504  /50x.html;
+    location = /50x.html {
+        root   html;
+    }
+    
+    location ~ \.php$ {
+        root           html;
+        fastcgi_pass   127.0.0.1:9000;
+        fastcgi_index  index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+
+        include        fastcgi_params;
+    }
+}
+EOF
+
+cat <<'EOF' > ${install_path}/service/install.sh 
+#!/usr/bin/env bash
+
+# Add NGINX group and user if they do not already exist
+sudo id -g nginx &>/dev/null || sudo addgroup --system nginx
+sudo id -u nginx &>/dev/null || sudo adduser --disabled-password --system --shell /sbin/nologin --group nginx
+
+if [ ! -e "/lib/systemd/system/nginx.service" ]; then
+sudo cp /usr/local/nginx/service/nginx.service /lib/systemd/system/nginx.service
+sudo systemctl daemon-reload
+sudo systemctl enable nginx
+sudo systemctl start nginx
 fi
+EOF
+chmod +x ${install_path}/service/install.sh
 
-# Create NGINX systemd service file if it does not already exist
-if [ ! -e "${install_path}/service/nginx.service" ]; then
-  # Control will enter here if the NGINX service doesn't exist.
-  file="${install_path}/service/nginx.service"
-
-  mkdir -p ${file}
-  /bin/cat >$file <<'EOF'
+# Systemd service for NGINX if not exist
+cat <<'EOF' > ${install_path}/service/nginx.service
 [Unit]
 Description=The NGINX HTTP and reverse proxy server
 After=syslog.target network.target remote-fs.target nss-lookup.target
 
 [Service]
 Type=forking
-PIDFile=/usr/local/nginx/var/run/nginx.pid
+PIDFile=/usr/local/nginx/run/nginx.pid
 ExecStartPre=/usr/local/nginx/sbin/nginx -t
-ExecStart=/usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
+ExecStart=/usr/local/nginx/sbin/nginx
 ExecReload=/bin/kill -s HUP $MAINPID
 ExecStop=/bin/kill -s QUIT $MAINPID
 PrivateTmp=true
@@ -239,25 +280,8 @@ PrivateTmp=true
 [Install]
 WantedBy=multi-user.target
 EOF
-fi
 
-# Create NGINX systemd service file if it does not already exist
-if [ ! -e "${install_path}/service/README" ]; then
-  # Control will enter here if the NGINX service doesn't exist.
-  file="${install_path}/service/README"
+chown -R nginx:nginx ${install_path}
 
-  mkdir -p ${file}
-  /bin/cat >$file <<'EOF'
-
-# Add NGINX group and user if they do not already exist
-id -g nginx &>/dev/null || addgroup --system nginx
-id -u nginx &>/dev/null || adduser --disabled-password --system --shell /sbin/nologin --group nginx
-
-cp /usr/local/nginx/service/nginx.service /lib/systemd/system/nginx.service
-systemctl start nginx.service
-EOF
-fi
-
-
-echo "All done.";
-echo "pls read ${install_path}/README"
+# Print completion message
+echo "NGINX has been installed and started successfully."
